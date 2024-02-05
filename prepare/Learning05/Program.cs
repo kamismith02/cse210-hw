1ba5d9d486d13288f74c8876c2beb918046699a5
kamismith02 <kamilou0709@gmail.com>
2024-02-05 15:38:09 -0700

    shape prep activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.ObjectModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square = new Square("Red", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 10);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Yellow", 5);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"The {s.GetColor()} shape has an area of {s.GetArea()}.");
+        }
     }
 }
